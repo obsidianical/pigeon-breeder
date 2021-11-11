6b9be5e2515a6f242186598a8250e4d5c12a7164
Kaiser <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2021-11-11 10:42:57 +0100

    Update

--- code/creature.py
+++ code/creature.py
@@ -4,45 +4,14 @@ class creature:
     def __init__(self, uid, name, sex, parents:list=None):
         self.uid = uid
         self.name = name
-        self.isFemale = sex
-        self.parents = parents
-        self.relations = dict()
-        """
-        {
-            "partner":partner,
-            "children":dict(),
-            "isActive":bool()
-        }
-        """
-        self.currentRelation = None
-        self.children = dict() #Dictionary of all children
 
-        self.isInRelation = False
+        self.isFemale = sex
         self.canReproduce = True
         self.isAlive = True
 
-    def determinSpecies(self):
-        
-        pass
+        self.parents = parents
+        self.children = dict() #Dictionary of all children
 
-    def checkIfInRelation(self):
-        if isEmpty(self.relations):
-            return False #If the dictionary is empty return that they are not in a relationship
-
-        for relation in self.relations:
-            if relation["partner"].isAlive == False and relation["isActive"] == True:
-                relation["isActive"] = False #Sets as false if the partner is dead, fail safe should been missed elsewhere
-
-            if relation["isActive"] == True:
-                self.currentRelation = relation
-                relation["partner"].currentRelation = relation
-
-                return True
-
-        return False
-
-    def addChild(self, relation, child):
+    def addChild(self, child):
         #Adds child where needed
-        relation["children"][str(child.uid)] = child
-        relation["partner"].children[str(child.uid)] = child
         self.children[str(child.uid)] = child
--- code/daycare.py
+++ code/daycare.py
@@ -1,4 +1,6 @@
 from creature import *
+from common import *
+from random import getrandbits, randint
 
 class daycare:
     def __init__(self, name):
@@ -6,14 +8,42 @@ class daycare:
 
         self.creatures = dict()
         self.historicCreatures = dict()
+        self.breedingGroups = dict()
+        """
+        breedingGroups = {
+            groupUID:{
+                "all":[List of creatures],
+                "male":[List of males],
+                "female":[List of females]
+            }
+        }
+        """
 
-    def getUID(self):
+    def getCreatureUID(self):
         return len(self.historicCreatures)
 
+    def getGroupUID(self):
+        return len(self.breedingGroups)
+
     def createCreature(self, name, sex, parents:list=None):
-        newCreature = creature(self.getUID, name, sex, parents)
+        newCreature = creature(self.getCreatureUID(), name, sex, parents)
         self.historicCreatures[str(newCreature.uid)] = newCreature
         self.creatures[str(newCreature.uid)] = newCreature
+
+        return newCreature
+
+    def generateRandomCreature(self):
+        randomCreature = self.createCreature("Randy", getrandbits(1))
+
+        return randomCreature
+
+    def updateGroups(self):
+        for group in self.breedingGroups:
+            for pigeon in group["all"]:
+                
+                pass
+            pass
+        pass
 
     def reproduce(self, target):
 
