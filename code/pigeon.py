2b62ac41e7da4ef8bf5bb01e0e6b21ef44c90ebd
Kaiser <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2021-11-22 19:14:14 +0100

    Replaced Spaces
    Where did these fuckers come from?
    This is a tab household and we only use tabs!

--- code/pigeon.py
+++ code/pigeon.py
@@ -2,17 +2,17 @@ from common import *
 import textwrap as tw
 
 class pigeonClass:
-    def __init__(self, pigeonUID, name, sex, parents:list=None):
-        self.uid = pigeonUID
-        self.name = name
-        self.age = 0 # Age in months
+	def __init__(self, pigeonUID, name, sex, parents:list=None):
+		self.uid = pigeonUID
+		self.name = name
+		self.age = 0 # Age in months
 
-        self.isFemale = sex
-        self.canReproduce = True
-        self.isAlive = True
-        self.timesBreed = 0
+		self.isFemale = sex
+		self.canReproduce = True
+		self.isAlive = True
+		self.timesBreed = 0
 
-        self.didAct = True
+		self.didAct = True
 
 		self.genetics = {
 			"fluff":1,
@@ -20,30 +20,30 @@ class pigeonClass:
 			"size":1
 		}
 
-        self.parents = parents
-        self.children = dict() #Dictionary of all children
+		self.parents = parents
+		self.children = dict() #Dictionary of all children
 
-    def addChild(self, child):
-        #Adds child where needed
-        self.children[str(child.uid)] = child
+	def addChild(self, child):
+		#Adds child where needed
+		self.children[str(child.uid)] = child
 
-    def getGender(self):
-        return "Female" if self.isFemale else "Male"
+	def getGender(self):
+		return "Female" if self.isFemale else "Male"
 
-    def show(self):
-        parents = ""
-        if self.parents != None:
-            for parent in self.parents:
-                parents += f"Parent: {parent.uid}\n"
+	def show(self):
+		parents = ""
+		if self.parents != None:
+			for parent in self.parents:
+				parents += f"Parent: {parent.uid}\n"
 
-        stringyBoi = f"""\
-            UID: {self.uid}
-            Name: {self.name}
-            Age: {self.age} Months
-            Gender: {self.getGender()}
-            Alive: {self.isAlive} {parents}
-            Fluffiness: {self.genetics["fluff"]}
-            Size: {self.genetics["size"]}
-            Speed: {self.genetics["speed"]}"""
+		stringyBoi = f"""\
+			UID: {self.uid}
+			Name: {self.name}
+			Age: {self.age} Months
+			Gender: {self.getGender()}
+			Alive: {self.isAlive} {parents}
+			Fluffiness: {self.genetics["fluff"]}
+			Size: {self.genetics["size"]}
+			Speed: {self.genetics["speed"]}"""
 
-        return tw.dedent(stringyBoi)
+		return tw.dedent(stringyBoi)
