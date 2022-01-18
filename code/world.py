ed1af20cad9a450c8756a8ddc2484ef609eaf580
Kaiser <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2022-01-18 21:02:56 +0100

    Update world.py
    TABS WHY DID IT USE SPACES

--- code/world.py
+++ code/world.py
@@ -1,44 +1,46 @@
 from json import load
 
 class World:
-    def __init__(self, savename:str, worldname:str, isDebugOn:bool=False):
-        self.savename = savename
-        self.worldname = worldname
+	def __init__(self, savename:str, worldname:str, isDebugOn:bool=False):
+		self.savename = savename
+		self.worldname = worldname
 
-        self.isDebugOn = isDebugOn
+		self.isDebugOn = isDebugOn
 
-        self.year = 0
-        self.month = 1
+		self.calendar = {
+			"month":1,
+			"year":0
+		}
 
-        self.wealth = 50
+		self.wealth = 50
 
-        self.randomNames = load(open("./input/pigeonNames.json", "r"))
-        self.help = open("./input/help.txt", "r").read()
-        self.genes = load(open("./input/genetics.json"))["geneBlocks"]
-        self.geneValues = ["fluff", "speed", "size"]
-        self.alleles = load(open("./input/genetics.json"))["possibleGenes"]
+		self.randomNames = load(open("./input/pigeonNames.json", "r"))
+		self.help = open("./input/help.txt", "r").read()
+		self.genes = load(open("./input/genetics.json"))["geneBlocks"]
+		self.geneValues = ["fluff", "speed", "size"]
+		self.alleles = load(open("./input/genetics.json"))["possibleGenes"]
 
-        self.species = dict() # A dictionary with all species
-        self.cares = dict() # A dictionary with all cares
-        self.arenas = dict() # A dictionary with all arenas
+		self.species = dict() # A dictionary with all species
+		self.cares = dict() # A dictionary with all cares
+		self.arenas = dict() # A dictionary with all arenas
 
-    def updateTime(self):
-        self.month += 1
+	def updateTime(self):
+		self.month += 1
 
-        if 12 < self.month:
-            self.month = 1
-            self.year += 1
+		if 12 < self.month:
+			self.month = 1
+			self.year += 1
 
-    def command(self, command):
-        command = command.lower().split()
+	def command(self, command):
+		command = command.lower().split()
 
-        match command[0]:
+		match command[0]:
 
-            case "help" | "h" | "?":
-                print(self.help)
+			case "help" | "h" | "?":
+				print(self.help)
 
-            case "quit" | "q":
-                raise EOFError
+			case "quit" | "q":
+				raise EOFError
 
-            case _:
-                print("Command not found")
+			case _:
+				print("Command not found")
