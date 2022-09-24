f295afc0e14b388dff31046915316c3371601043
German Nr°4 <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2022-09-24 08:19:29 +0200

    Little Test Program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{Write, stdout, stdin};
+
 fn main() {
     println!("Hello, world!");
+
+    stdout().flush(); 
+
+    let mut input = String::new(); // Creates new variable with type String
+    std::io::stdin().read_line(&mut input); // Takes user input
+
+    println!("{}", input); // Prints user input
+    
 }
