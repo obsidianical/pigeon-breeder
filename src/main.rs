cc429c6675ddafff4243115921f17e542bc7d8e9
German Nr°4 <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2022-09-27 15:43:56 +0200

    Added pigeons
    Added both a pigeon struct and impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
-use std::io::{Write, stdout, stdin};
+use std::io::{Write, stdout};
+
+struct Pigeon {
+    pub uid: String,
+    name: String,
+    age: i32,
+
+    is_female: bool,
+    can_reproduce: bool,
+    is_alive: bool,
+
+    did_act: bool
+}
+
+impl Pigeon {
+    pub fn new(uid: String, name: String, age: i32, is_female: bool, can_reproduce: bool, is_alive: bool, did_act: bool) -> Self {
+        Self {
+            uid,
+            name,
+            age, 
+            is_female,
+            can_reproduce,
+            is_alive,
+            did_act
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
@@ -8,6 +34,7 @@ fn main() {
     let mut input = String::new(); // Creates new variable with type String
     std::io::stdin().read_line(&mut input); // Takes user input
 
-    println!("{}", input); // Prints user input
-    
+    let pigeon_test = Pigeon::new("P1".to_string(), input.to_string(), 1, false, true, true, false);
+
+    println!("{}", pigeon_test.name); // Prints user input
 }
